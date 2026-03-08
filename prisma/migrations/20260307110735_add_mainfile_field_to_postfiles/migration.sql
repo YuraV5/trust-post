ddad61e6c9f1e90b7cb87a444ccd2f9f5d75bc4f
YuraVak <1vakyura1@gmail.com>
2026-03-08 10:36:20 +0100

    feat: initialize database schema with user, post, and file management tables

--- prisma/migrations/20260307110735_add_mainfile_field_to_postfiles/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "post_files" ADD COLUMN     "main_image" BOOLEAN NOT NULL DEFAULT false;
-
--- CreateIndex
-CREATE INDEX "post_files_post_id_main_image_idx" ON "post_files"("post_id", "main_image");

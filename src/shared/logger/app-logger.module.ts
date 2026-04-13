235cfa310e8740cf8633f9c2335eacb7e7ab7e06
YuraV5 <1vakyura1@gmail.com>
2026-04-13 13:19:46 +0200

    separate logger configuration into LoggerConfigService (#58)

--- src/shared/logger/app-logger.module.ts
+++ src/shared/logger/app-logger.module.ts
@@ -1,9 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { APP_LOGGER, AppLogger } from './services/app-logger';
+import { LoggerConfigService } from './services/logger-config.service';
 
 @Global()
 @Module({
   providers: [
+    LoggerConfigService,
     {
       provide: APP_LOGGER,
       useClass: AppLogger,
